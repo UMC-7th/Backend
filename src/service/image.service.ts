58c26129a0342c363a369e8d07b215c8a213bd05
ohige01 <ohige01@naver.com>
2025-01-15 23:52:07 +0900

    [feat] #10 이미지 검색 결과 개선

--- src/service/image.service.ts
+++ src/service/image.service.ts
@@ -5,9 +5,9 @@ export const createFoodImageService = async (name: string) => {
     try {
         //Naver API를 이용하여 이미지 검색
         const result = await axios.get(
-          "https://openapi.naver.com/v1/search/image.json",
+          "https://openapi.naver.com/v1/search/shop.json",
           {
-            params: { query: name, display: 1 },
+            params: { query: name, display: 5 },
             headers: {
               'X-Naver-Client-Id':
               process.env.NAVER_CLIENT_ID,  // 환경변수로 설정한 NAVER_CLIENT_ID
@@ -17,7 +17,7 @@ export const createFoodImageService = async (name: string) => {
           }
         );
         //검색 결과에서 이미지 url을 추출하여 반환
-        return result.data.items[0].thumbnail;
+        return result.data.items[3].image;
     
       } catch (error) {
         throw new Error("이미지 생성 실패");
