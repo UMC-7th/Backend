935f44a7e1dc10f8d7d46cc0e408f6048fb66f1d
ohige01 <ohige01@naver.com>
2025-01-28 01:52:55 +0900

    [FEAT] #43 배송지 정보 조회 api 구현

--- src/controller/deliveryAddress.controller.ts
+++ src/controller/deliveryAddress.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { addDeliveryAddressService } from "../service/deliveryAddress.service.js";
+import { addDeliveryAddressService, getDeliveryAddressService } from "../service/deliveryAddress.service.js";
 import { StatusCodes } from "http-status-codes";
 import { InvalidInputError } from "../util/error.js";
 
@@ -17,6 +17,25 @@ export const addDeliveryAddress = async (
         res.status(StatusCodes.OK).success({
             isSuccess: true,
             addressId: deliveryAddress.addressId
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+//배송지 조회
+export const getDeliveryAddress = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const userId = req.user?.id;
+    try {
+        if (!userId) 
+            throw new InvalidInputError("잘못된 토큰 값입니다.", "입력 값: " + req.headers.authorization);
+        const deliveryAddressList = await getDeliveryAddressService(userId);
+        res.status(StatusCodes.OK).success({
+            result: deliveryAddressList
         });
     } catch (error) {
         next(error);
--- src/service/deliveryAddress.service.ts
+++ src/service/deliveryAddress.service.ts
@@ -1,5 +1,5 @@
 import { addDeliveryAddressReq } from "../dto/deliveryAddress.dto.js";
-import { addDeliveryAddress } from "../repository/deliveryAddress.repository.js";
+import { addDeliveryAddress, getDeliveryAddress } from "../repository/deliveryAddress.repository.js";
 
 //배송지 DB에 저장
 export const addDeliveryAddressService = async (
@@ -8,4 +8,10 @@ export const addDeliveryAddressService = async (
 ) => {
     const deliveryAddress = await addDeliveryAddress(userId, data);
     return deliveryAddress;
+};
+
+//배송지 DB에서 조회
+export const getDeliveryAddressService = async (userId: number) => {
+    const deliveryAddressList = await getDeliveryAddress(userId);
+    return deliveryAddressList;
 };
