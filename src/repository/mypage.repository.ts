8771b6469995b1deda72f06ccf827345b66f98d1
namkungkang <namkung0131@naver.com>
2025-01-30 06:21:53 +0900

    [FEAT] #28 유저 회원탈퇴 api 구현

--- src/repository/mypage.repository.ts
+++ src/repository/mypage.repository.ts
@@ -4,17 +4,28 @@ import { DBError } from "../util/error.js";
 export const findUserProfile = async (userId: number) => {
   try {
     const user = await prisma.user.findFirst({
-      where: { userId: userId },  
+      where: { userId: userId },
       select: {
-        nickname:true,
-        name: true,        
-        email: true,       
-        birth: true,       
-        phoneNum: true     
+        nickname: true,
+        name: true,
+        email: true,
+        birth: true,
+        phoneNum: true,
       },
     });
-    
+
     return user;
+  } catch (error) {
+    throw new DBError("사용자 조회 중 오류가 발생했습니다.", error);
+  }
+};
+
+export const deleteUserProfile = async (userId: number) => {
+  try {
+    const deleteUser = await prisma.user.delete({
+      where: { userId: userId },
+    });
+    return deleteUser;
   } catch (error) {
     throw new DBError("사용자 조회 중 오류가 발생했습니다.", error);
   }
