886e1c81c67a198b53c0bf9dce79c48ab3970520
ohige01 <ohige01@naver.com>
2025-02-01 17:32:24 +0900

    [FEAT] #81 식재료 북마크 삭제 구현

--- src/controller/material.controller.ts
+++ src/controller/material.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { InvalidInputError } from "../util/error.js";
-import { getMarkMaterialListService, markMaterialService } from "../service/material.service.js";
+import { deleteMarkMaterialService, getMarkMaterialListService, markMaterialService } from "../service/material.service.js";
 
 //식재료 북마크 추가
 export const markMaterial = async (
@@ -36,6 +36,26 @@ export const getMarkMaterialList = async (
         const markList = await getMarkMaterialListService(userId);
         res.status(StatusCodes.OK).success({
             markList: markList
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+//식재료 북마크 삭제
+export const deleteMarkMaterial = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const userId = req.user?.id;
+    try {
+        if (!userId) 
+            throw new InvalidInputError("잘못된 토큰 값입니다.", "입력 값: " + req.headers.authorization);
+        const markMaterial = await deleteMarkMaterialService(userId, req.body.material);
+        res.status(StatusCodes.OK).success({
+            isSuccess: true,
+            markMaterial: markMaterial
         });
     } catch (error) {
         next(error);
--- src/routes/material.route.ts
+++ src/routes/material.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { jwtAuthMiddleware } from "../util/jwt.middleware.js";
-import { getMarkMaterialList, markMaterial } from "../controller/material.controller.js";
+import { deleteMarkMaterial, getMarkMaterialList, markMaterial } from "../controller/material.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post("/mark", jwtAuthMiddleware, markMaterial);
 //식재료 북마크 조회(리스트)
 router.get("/mark", jwtAuthMiddleware, getMarkMaterialList);
+//식재료 북마크 삭제
+router.delete("/mark", jwtAuthMiddleware, deleteMarkMaterial);
 
 export default router;
