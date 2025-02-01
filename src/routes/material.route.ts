be9c55a5d819da38cc1bc6a9d116a5a46f81ba47
ohige01 <ohige01@naver.com>
2025-02-01 17:14:37 +0900

    [FEAT] #79 식재료 북마크 조회 구현

--- src/routes/material.route.ts
+++ src/routes/material.route.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { jwtAuthMiddleware } from "../util/jwt.middleware.js";
-import { markMaterial } from "../controller/material.controller.js";
+import { getMarkMaterialList, markMaterial } from "../controller/material.controller.js";
 
 const router = express.Router();
 
 //식재료 북마크 추가
 router.post("/mark", jwtAuthMiddleware, markMaterial);
+//식재료 북마크 조회(리스트)
+router.get("/mark", jwtAuthMiddleware, getMarkMaterialList);
 
 export default router;
