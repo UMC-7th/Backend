6ced6949cf612573b1c53c28d28730f6af8b235f
ohige01 <ohige01@naver.com>
2025-02-01 16:58:34 +0900

    [FEAT] #77 식재료 북마크 중복 처리

--- src/repository/material.repository.ts
+++ src/repository/material.repository.ts
@@ -1,6 +1,21 @@
 import { prisma } from "../db.config.js";
 import { DBError } from "../util/error.js";
 
+//식재료 북마크 단일 조회
+export const getMarkMaterial = async (userId: number, material: string) => {
+    try {
+        const mark = await prisma.markMaterial.findFirst({
+            where: {
+                userId: userId,
+                material: material
+            }
+        });
+        return mark;
+    } catch (error) {
+        throw new DBError("식재료 북마크 조회 중 오류가 발생했습니다.", error);
+    }
+};
+
 //식재료 북마크 추가
 export const markMaterial = async (userId: number, material: string) => {
     try {
--- src/service/material.service.ts
+++ src/service/material.service.ts
@@ -1,7 +1,13 @@
-import { markMaterial } from "../repository/material.repository.js";
+import { getMarkMaterial, markMaterial } from "../repository/material.repository.js";
+import { InvalidInputError } from "../util/error.js";
 
 //식재료 북마크 추가
 export const markMaterialService = async (userId: number, material: string) => {
+    const confirm = await getMarkMaterial(userId, material);
+    if (confirm) {
+        throw new InvalidInputError("이미 북마크된 식재료입니다.", `userId: ${userId}, material: ${material}`);
+    }
+    
     const mark = await markMaterial(userId, material);
     return mark;
 }
