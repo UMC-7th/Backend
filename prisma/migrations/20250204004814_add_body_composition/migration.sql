efe66a9896e0ac570042982218224f8f3500da60
namkungkang <namkung0131@naver.com>
2025-02-04 09:57:56 +0900

    [FEAT] #88 컬럼 추가

---
+++ prisma/migrations/20250204004814_add_body_composition/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `survey` ADD COLUMN `body_fat_percentage` DOUBLE NULL,
+    ADD COLUMN `healthConditionDetails` VARCHAR(200) NULL,
+    ADD COLUMN `skeletal_Mucle_Mass` DOUBLE NULL;
